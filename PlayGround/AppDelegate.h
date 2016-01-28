9d525409360bcf51a945464119584e97eac0c8c9
daniel.miau <daniel.miau@gmail.com>
2016-01-28 18:07:22 -0500

    Initial Commit

---
+++ PlayGround/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  PlayGround
+//
+//  Created by dmiau on 1/28/16.
+//  Copyright © 2016 dmiau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ PlayGround/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PlayGround
+//
+//  Created by dmiau on 1/28/16.
+//  Copyright © 2016 dmiau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
