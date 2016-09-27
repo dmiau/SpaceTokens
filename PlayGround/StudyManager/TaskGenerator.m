566426095f18cb0221d543d801e1dc956452b5f4
daniel.miau <daniel.miau@gmail.com>
2016-09-27 14:37:39 +0100

    - generate anchor+place tasks

--- PlayGround/StudyManager/TaskGenerator.m
+++ PlayGround/StudyManager/TaskGenerator.m
@@ -9,6 +9,7 @@
 #import "TaskGenerator.h"
 #import <MapKit/MapKit.h>
 #import "SnapshotPlace.h"
+#import "SnapshotAnchorPlus.h"
 #import "CustomMKMapView.h"
 
 @implementation TaskGenerator
@@ -32,11 +33,14 @@
     NSMutableArray* controlArray = [[NSMutableArray alloc] init];
     NSMutableArray* experimentArray = [[NSMutableArray alloc] init];
     
-    NSMutableDictionary *taskDictionary = [self generatePlaceDictionary];
     
+    //-------------------
+    // Generate tasks for PLACE
+    //-------------------
+    NSMutableDictionary *placeTaskDictionary = [self generatePlaceDictionary];
     // Need to handle multiple conditions
-    for (NSString *aKey in taskDictionary){
-        SnapshotPlace *snapshot = taskDictionary[aKey];
+    for (NSString *aKey in placeTaskDictionary){
+        SnapshotPlace *snapshot = placeTaskDictionary[aKey];
         SnapshotPlace *snapshotCopy = [snapshot copy];
         
         snapshot.name = [snapshot.name stringByAppendingString:@":control"];
@@ -48,6 +52,28 @@
         [experimentArray addObject:snapshotCopy];
     }
  
+    //-------------------
+    // Generate tasks for ANCHOR
+    //-------------------
+    NSMutableDictionary *anchorTaskDictionary = [self generateAnchorPlusDictionary];
+    // Need to handle multiple conditions
+    for (NSString *aKey in anchorTaskDictionary){
+        SnapshotPlace *snapshot = anchorTaskDictionary[aKey];
+        SnapshotPlace *snapshotCopy = [snapshot copy];
+        
+        snapshot.name = [snapshot.name stringByAppendingString:@":control"];
+        snapshot.condition = CONTROL;
+        [controlArray addObject:snapshot];
+        
+        snapshotCopy.name = [snapshotCopy.name stringByAppendingString:@":experiment"];
+        snapshotCopy.condition = EXPERIMENT;
+        [experimentArray addObject:snapshotCopy];
+    }
+    
+
+    //-------------------
+    // Assemble the task vector
+    //-------------------
     NSMutableArray *taskArray = [[NSMutableArray alloc] init];
     [taskArray addObjectsFromArray:controlArray];
     [taskArray addObjectsFromArray:experimentArray];
@@ -87,6 +113,8 @@
                                       @"west": @180, @"southwest": @225,
                                       @"south": @270, @"southeast": @315};
     
+    CustomMKMapView *mapView = [CustomMKMapView sharedManager];
+    
     // 8 directions
     for (NSString *aKey in [angleDictionary allKeys]){
         
@@ -103,7 +131,8 @@
         [placeSnapshot.poisForSpaceTokens addObject:tokenPOI];
         
         // Assemble the POI for the target area
-        POI *target = [self generateTargetForTokenPOI:tokenPOI withAngle:degree];
+        POI *target = [self generateTargetForReferencePOI:tokenPOI withAngle:degree
+                                           offSetDistance:20+ mapView.frame.size.width * 0.4];
         
         [placeSnapshot.targetedPOIs addObject:target];
         
@@ -117,10 +146,82 @@
     return outDictionary;
 }
 
+//=====================
+// Method to generate tasks for Anchor + Place
+//=====================
+- (NSMutableDictionary<NSString*, SnapshotAnchorPlus*> * )generateAnchorPlusDictionary{
+    NSMutableDictionary *outDictionary = [[NSMutableDictionary alloc] init];
+    
+    //---------------------
+    // Initial condition
+    //---------------------
+    
+    // London City Hall
+    MKCoordinateRegion initRegion =
+    MKCoordinateRegionMake(
+                           CLLocationCoordinate2DMake(51.5044, -0.0772236),
+                           MKCoordinateSpanMake(0.00432843, 0.00504386));
+    
+    //---------------------
+    // Create an anchor (at centroid)
+    //---------------------
+    
+    // Grand Central for now
+    POI *anchorPOI = [[POI alloc] init];
+    anchorPOI.name = @"London City Hall";
+    anchorPOI.latLon = initRegion.center;
+    anchorPOI.coordSpan = initRegion.span;
+    
+    
+    //---------------------
+    // Create a target SpaceToken POI (one for each snapshot)
+    //---------------------
+    NSDictionary *angleDictionary = @{@"east": @0, @"northeast": @45,
+                                      @"north": @90, @"northwest": @135,
+                                      @"west": @180, @"southwest": @225,
+                                      @"south": @270, @"southeast": @315};
+    
+    CustomMKMapView *mapView = [CustomMKMapView sharedManager];
+    
+    // 8 directions
+    for (NSString *aKey in [angleDictionary allKeys]){
+        
+        double degree = [angleDictionary[aKey] doubleValue];
+        
+        // Create a snapshot
+        SnapshotAnchorPlus *anchorSnapshot = [[SnapshotAnchorPlus alloc] init];
+        
+        // Set the initial condition
+        anchorSnapshot.latLon = initRegion.center;
+        anchorSnapshot.coordSpan = initRegion.span;
+        
+        // Assemble the POI for the anchor
+        [anchorSnapshot.highlightedPOIs addObject:anchorPOI];
+        
+        // Assemble the POI for the spacetoken
+        POI *token = [self generateTargetForReferencePOI:anchorPOI withAngle:degree
+                                           offSetDistance:mapView.frame.size.height * 2];
+        token.name = @"Hotel";
+        [anchorSnapshot.poisForSpaceTokens addObject:token];
+        
+        // Assemble the POI for the targeted area
+        [anchorSnapshot.targetedPOIs addObject:anchorPOI];
+        [anchorSnapshot.targetedPOIs addObject:token];
+        
+        // Generate the ID and instructions
+        anchorSnapshot.name = [NSString stringWithFormat:@"ANCHOR:%@", aKey];
+        anchorSnapshot.instructions =
+        [NSString stringWithFormat:@"Inspect the area between the anchor and hotel."];
+        outDictionary[anchorSnapshot.name] = anchorSnapshot;
+    }
+    
+    return outDictionary;
+}
+
 //----------------
 // Generate a target POI
 //----------------
-- (POI*)generateTargetForTokenPOI: (POI*) tokenPOI withAngle: (double)degree{
+- (POI*)generateTargetForReferencePOI: (POI*) tokenPOI withAngle: (double)degree offSetDistance: (double) offset{
     POI* outPOI = [[POI alloc] init];
     
     CustomMKMapView *mapView = [CustomMKMapView sharedManager];
@@ -130,7 +231,7 @@
     
     // Calculate the desired rect, then the desired region
     double diameter = mapView.frame.size.width * 0.8;
-    double arm = 20 + diameter/2;
+    double arm = offset; //20 + diameter/2;
     
     // Calculate the centroid (in CGPoint) of the region
     CGPoint viewCentroid = CGPointMake(mapView.frame.size.width/2, mapView.frame.size.height/2);
