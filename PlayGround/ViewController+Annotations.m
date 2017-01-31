bcf9c0be79fa85ca29cb32476ceee127d4db3de9
Daniel Miau <daniel.miau@gmail.com>
2017-01-30 22:16:09 -0500

    Update Google Maps SDK to 2.1

--- PlayGround/ViewController+Annotations.m
+++ PlayGround/ViewController+Annotations.m
@@ -31,29 +31,29 @@ didTapAtCoordinate:	(CLLocationCoordinate2D) coordinate
     [[AnnotationCollection sharedManager] resetAnnotations];
 }
 
-- (void)mapView:(GMSMapView *)mapView
-didTapPOIWithPlaceID:(NSString *)placeID
-           name:(NSString *)name
-       location:(CLLocationCoordinate2D)location
-{
-    NSLog(@"You tapped %@: %@, %f/%f", name, placeID, location.latitude, location.longitude);
-}
-
 //- (void)mapView:(GMSMapView *)mapView
 //didTapPOIWithPlaceID:(NSString *)placeID
 //           name:(NSString *)name
 //       location:(CLLocationCoordinate2D)location
 //{
-//    GMSMarker *infoMarker;
-//    infoMarker = [GMSMarker markerWithPosition:location];
-//    infoMarker.snippet = placeID;
-//    infoMarker.title = name;
-//    infoMarker.opacity = 0;
-//    CGPoint pos = infoMarker.infoWindowAnchor;
-//    pos.y = 1;
-//    infoMarker.infoWindowAnchor = pos;
-//    infoMarker.map = mapView;
-//    mapView.selectedMarker = infoMarker;
+//    NSLog(@"You tapped %@: %@, %f/%f", name, placeID, location.latitude, location.longitude);
 //}
+
+GMSMarker *infoMarker;
+- (void)mapView:(GMSMapView *)mapView
+didTapPOIWithPlaceID:(NSString *)placeID
+           name:(NSString *)name
+       location:(CLLocationCoordinate2D)location
+{    
+    infoMarker = [GMSMarker markerWithPosition:location];
+    infoMarker.snippet = placeID;
+    infoMarker.title = name;
+    infoMarker.opacity = 0;
+    CGPoint pos = infoMarker.infoWindowAnchor;
+    pos.y = 1;
+    infoMarker.infoWindowAnchor = pos;
+    infoMarker.map = mapView;
+    mapView.selectedMarker = infoMarker;
+}
 
 @end
